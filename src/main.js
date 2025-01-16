a65a3e4686c0693aa2fe0e3b362f1828e550e9a2
Diana Zinevych <dianazinevych02@gmail.com>
2025-01-16 16:24:07 +0200

    add import in main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchPhotosByQuery } from './js/pixabay-api';
+import { createGalleryCardTemplate } from './render-function';
 
 const serchFormEl = document.querySelector('search-input');
 const btnFormEl = document.querySelector('search-btn');
