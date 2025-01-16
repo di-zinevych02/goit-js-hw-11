0a7c1a158fcd1a0471e2b183567485e8e829fa78
Diana Zinevych <dianazinevych02@gmail.com>
2025-01-16 16:12:01 +0200

    done function in  fetch

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchPhotosByQuery } from './js/pixabay-api';
+
+const serchFormEl = document.querySelector('search-input');
+const btnFormEl = document.querySelector('search-btn');
