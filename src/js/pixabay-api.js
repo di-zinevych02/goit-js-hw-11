4b3e3fa1f51d4b8e2cfca7258fdec0485cac932b
Diana Zinevych <dianazinevych02@gmail.com>
2025-01-19 19:37:56 +0100

    add render-function

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,4 @@
-const API_KEY = '48264002-554f1b79d0a5e107f7aa1f349';
+
 export const fetchPhotosByQuery = searchedQuery => {
   const searchParams = new URLSearchParams({
     key: '48264002-554f1b79d0a5e107f7aa1f349',
--- src/main.js
+++ src/main.js
@@ -9,13 +9,49 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchPhotosByQuery } from './js/pixabay-api';
 import { createGalleryCardTemplate } from './js/render-function';
 
-const serchFormEl = document.querySelector('search-input');
-const btnFormEl = document.querySelector('search-btn');
+const serchFormEl = document.querySelector('.js-search-form');
+const btnFormEl = document.querySelector('.search-btn');
+const galleryEl = document.querySelector('.js-gallery');
+
 const onSearchFormSubmit = event => {
   event.preventDefault();
-  const searchedQuery = event.currentTarget.elements.value;
-  
-};
 
-btnFormEl.addEventListener("submit", onSearchFormSubmit);
+  const searchedQuery = event.currentTarget.elements.user_query.value.trim();
+
+  if (searchedQuery === "") {
+    iziToast.warning({
+      message: 'Please enter a search term.',
+      position: 'topRight',
+    });
+
+    return;
+  }
+
+  fetchPhotosByQuery(searchedQuery)
+    .then(data => {
+      if (data.total === 0) {
+        iziToast.info({
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        galleryEl.innerHTML = '';
+
+        searchFormEl.reset();
+
+        return;
+      }
+      const galleryTemplate = data.hits.map(el => createGalleryCardTemplate(el)).join('');
+      console.log(galleryTemplate);
+
+      galleryEl.innerHTML = galleryTemplate;
+    })
+    .catch(err => {
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong, please try again later.',
+        position: 'topRight',
+      });
+    });
+};
+serchFormEl.addEventListener('submit', onSearchFormSubmit);
   
